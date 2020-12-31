b37f839e039b28ae7e70fe10287986f9316b69ee
Andrew Elmore <andrewelmore93@gmail.com>
2020-12-30 23:41:23 -0500

    first commit

---
+++ frontend/index.js
@@ -0,0 +1,46 @@
+import {
+    initializeBlock,
+    useBase,
+    useRecords,
+    TablePickerSynced,
+    useGlobalConfig,
+} from '@airtable/blocks/ui';
+import React from 'react';
+import { getRecords, setRecords, findChangedRecord } from './records'
+
+
+function Spellcheck() {
+    const base = useBase();
+    const globalConfig = useGlobalConfig();
+    const tableId = globalConfig.get('selectedTableId');
+    const table = base.getTableByIdIfExists(tableId);
+    const records = useRecords(table);
+
+    console.log(records[0])
+    console.log(Object.values(records))
+    // if (records === getRecords()) {
+    //     console.log('no change', records)
+    // } else {
+    //     console.log("false 30 records: ", records)
+    //     console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    //     const storedRecords = getRecords()
+    //     console.log("false 33 storedRecords: ", storedRecords)
+    //     console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    //     setRecords(records)
+    //     const newStoredRecords = getRecords()
+    //     console.log("false 37 newStoredRecords: ", newStoredRecords)
+    //     console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    // }
+
+    // const changedRecord = findChangedRecord(record
+
+    // console.log(changedRecord)
+    // YOUR CODE GOES HERE
+    return (
+        <div>
+            <TablePickerSynced globalConfigKey="selectedTableId" />
+            Hello world 🚀
+        </div>);
+}
+
+initializeBlock(() => <Spellcheck />);
