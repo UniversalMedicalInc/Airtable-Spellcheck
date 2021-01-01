055708713ecb7dfa773a70d2ebd0f13a4ed6b735
Andrew Elmore <andrewelmore93@gmail.com>
2021-01-01 14:41:56 -0500

    remove server

---
+++ frontend/dictionary.js
@@ -0,0 +1,3 @@
+export default {
+    a5: ['apple']
+}
--- frontend/index.js
+++ frontend/index.js
@@ -7,6 +7,7 @@ import {
     useWatchable,
     useRecordById,
 } from '@airtable/blocks/ui';
+import axios from 'axios'
 import { cursor } from '@airtable/blocks';
 import React from 'react';
 
@@ -15,32 +16,24 @@ function Spellcheck() {
     const base = useBase();
     const globalConfig = useGlobalConfig();
     const table = base.getTableByName('Imported table');
-    // const records = useRecords(table);
+
     const defaultFieldId = table.getFieldByName("AUDIT").id
     const defaultRecordId = useRecords(table)[0].id
+
     const watchedFields = ['AUDIT'].map((field) => table.getFieldByName(field).id)
     useLoadable(cursor);
     useWatchable(cursor, ['selectedRecordIds', 'selectedFieldIds']);
     const fieldId = cursor.selectedFieldIds[0] || defaultFieldId
-    console.log("26 fieldId: ", fieldId)
-    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
     const recordId = cursor.selectedRecordIds[0] || defaultRecordId
-    console.log("29 recordId: ", recordId)
-    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
     const record = recordId ? useRecordById(table, recordId) : useRecords(table)[0]
-    console.log("32 record: ", record)
-    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
     const cell = fieldId ? record.getCellValue(fieldId) : record.getCellValue("TL_ID")
-    console.log("35 cell: ", cell)
-    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
 
     const field = watchedFields.includes(fieldId) ? <div>{cell}</div> : null;
 
-    // if (watchedFields.includes(cursor.selectedFieldIds[0])) {
-    //     console.log(record)
-    //     console.log(cell)
-
-    // } 
 
 
    
@@ -48,6 +41,7 @@ function Spellcheck() {
     return (
         <div>
             {field}
+            <button onClick={() => { axios.get('/')}}>fire</button>
         </div>);
 }
 
